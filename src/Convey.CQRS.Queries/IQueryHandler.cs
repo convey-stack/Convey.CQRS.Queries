b99197fb35b65683abd6fde50307f24ea0b32bcd
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-04-23 21:21:41 +0200

    TQuery constraint

--- src/Convey.CQRS.Queries/IQueryHandler.cs
+++ src/Convey.CQRS.Queries/IQueryHandler.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace Convey.CQRS.Queries
 {
-    public interface IQueryHandler<TQuery,TResult> where TQuery : class, IQuery<TResult>
+    public interface IQueryHandler<in TQuery,TResult> where TQuery : class, IQuery<TResult>
     {
         Task<TResult> HandleAsync(TQuery query);
     }
